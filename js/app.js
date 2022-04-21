f4feebb1686ab0e3b1b90ab9f06097ba201e238c
sainingo <nickmentor421@gmail.com>
2022-04-21 23:55:31 +0300

    Add Search functionality

--- js/app.js
+++ js/app.js
@@ -8,11 +8,38 @@ const fetchMovies = async () => {
         output += `
         <div class="movie-row">
         <img src="${movie.image.medium}" />
-        <h4>${movie.name}  <span>${movie.type}</span></h4>
+        <h4>${movie.name}  <span>${movie.season}</span></h4>
          </div>
         `;
         displayMoviesContainer.innerHTML = output;
     });
 }
 
+const searchMovies = async () => {
+    const searchButton = document.getElementById('search-btn');
+    const searchInput = document.getElementById('search');
+    searchButton.addEventListener('click', async () => {
+        displayMoviesContainer.innerHTML = '';
+        const searchValue = searchInput.value;
+        if (searchValue.trim() !== '') {
+            const searchData = await fetch(`https://api.tvmaze.com/search/shows?q=${searchValue}`)
+            const res = await searchData.json()
+            let searchOutput = "";
+            res.forEach((searchMovie) => {
+                    searchOutput += `
+                    <div class="movie-row">
+                   <img src="${searchMovie.show.image.medium}" />
+                  <h4>${searchMovie.show.name}  <span>${searchMovie.show.language}</span></h4>
+                  </div>
+                 `;
+                 displayMoviesContainer.innerHTML = searchOutput;
+                 searchInput.value = '';
+            });
+        }else {
+            displayMoviesContainer.innerHTML = 'Please Type the Name of the Movie'
+        }
+    })
+}
+
+searchMovies()
 fetchMovies()
